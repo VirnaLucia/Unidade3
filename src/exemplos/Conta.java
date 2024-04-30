509b3fd2cc0cb86a148a6eab6ab5be68a5fa9043
VirnaLucia <167825131+VirnaLucia@users.noreply.github.com>
2024-04-29 22:16:38 -0300

    Initial commit

---
+++ src/exemplos/Conta.java
@@ -0,0 +1,13 @@
+
+package exemplos;
+
+public class Conta {
+    int numero;
+    String nomeTitular;
+    double saldo;
+    void depositar(double valor) {
+        this.saldo = this.saldo + valor;
+        
+    }
+    
+}

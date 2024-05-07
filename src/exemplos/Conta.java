d4510013461399a2757d1190297956fc0197bc89
VirnaLucia <167825131+VirnaLucia@users.noreply.github.com>
2024-05-06 22:00:38 -0300

    Alteraçôes
    alterações de arquivos

--- src/exemplos/Conta.java
+++ src/exemplos/Conta.java
@@ -1,13 +1,40 @@
-
 package exemplos;
 
 public class Conta {
-    int numero;
-    String nomeTitular;
-    double saldo;
-    void depositar(double valor) {
+
+    private int numero;
+    private String nomeTitular;
+    private double saldo;
+
+    public void depositar(double valor) {
         this.saldo = this.saldo + valor;
-        
+
     }
-    
-}
+
+    public boolean sacar(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+        public double getSaldo(){
+            return this.saldo;
+        }
+        public int getnumero(){
+            return this.numero;
+        } 
+        
+        public String getNomeTitular() {
+        return this.nomeTitular;
+    }
+        public void setNumero(int numero){
+            this.numero = numero;
+        }
+        public void setNomeTitular(String nomeTitular){
+            this.nomeTitular = nomeTitular;
+        }
+    }
+
+
--- src/exemplos/TesteConta.java
+++ src/exemplos/TesteConta.java
@@ -7,7 +7,17 @@ public class TesteConta {
        Conta c = new Conta();
        c.depositar(100);
        c.depositar(50);
-        System.out.println("Saldo: " + c.saldo);
+       boolean saqueEfetuado = c.sacar(300);
+       if(saqueEfetuado){
+           System.out.println("Saque efetuado com sucesso!");
+       } else {
+           System.err.println("ERRO AO SACAR !!!");
+       }
+        System.out.println("Nome Titular: " + c.getNomeTitular());
+        System.out.println("Numero: " + c.getnumero());
+       System.out.println("Saldo: " + c.getSaldo()); 
+       c.getNomeTitular();
+       c.getnumero();
     }
+    }  
     
-}
